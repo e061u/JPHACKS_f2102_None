a7cfa59440f2e9a4df68868089a5e5154720b7aa
e061u <83745158+e061u@users.noreply.github.com>
2021-10-25 13:26:18 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = "Hoge"
+    #return name
+    return render_template('hello.html', title='flask test', name=name) #変更
+
+if __name__ == "__main__":
+    app.run(debug=True)
